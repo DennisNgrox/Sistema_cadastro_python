310c7f204a2a8b3258677cc6939066131851cf7b
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-12 21:49:48 -0300

    Create __init__.py
    Menu do programa

---
+++ Sistema/menu/__init__.py
@@ -0,0 +1,43 @@
+def escreva(txt):
+    print('-' * 36)
+    print(txt)
+    print('-' * 36)
+
+def opcao():
+    escreva('           MENU PRINCIPAL           ')
+
+    print('1 - Ver pessoas cadastradas')
+    print('2 - Cadastrar nova Pessoa')
+    print('3 - Sair do Sistema')
+    print('------------------------------------')
+    number = int(input('Sua Opção: '))
+    
+    if number == 1:
+        from time import sleep
+        import base
+        base.leitura()
+        sleep(3)
+        opcao()
+    elif number == 2:
+        from time import sleep
+        import registro
+        escreva('           CADASTRAR           ')
+        nome = str(input('Digite o nome que deseja cadastrar: '))
+        idade = str(input(f'Digite a idade do(a) {nome}: '))
+        registro.cadastrar(nome, idade)
+        print(f'{nome} cadastrado com a idade {idade} no Sistema')
+        sleep(3)
+        opcao()
+
+    elif number == 3:
+        try:
+            escreva('           FINALIZANDO           ')
+            from time import sleep
+            finalizar = 'Até maaais veeeerrr'
+        finally:
+            print(finalizar)
+
+   
+
+
+
