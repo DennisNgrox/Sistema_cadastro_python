a46f55b978fade59b55318a61fd8a07783cdc59a
Dennis Silva <81188924+DennisNgrox@users.noreply.github.com>
2023-07-12 22:20:57 -0300

    Update __init__.py

--- Sistema/registro/__init__.py
+++ Sistema/registro/__init__.py
@@ -1,6 +1,6 @@
 def cadastrar(nome, idade):
     import base
-    with open('C:\\Users\\Ageri\\Desktop\\SCRIPTS\\Python\\cadastro\\base\\base.txt', "a") as arquivo:
+    with open('\\base\\base.txt', "a") as arquivo: # Editar caminho atual para caminho completo
         a = arquivo.write(f'{nome} : {idade}\n')
         return f'Nome {nome} com a idade {idade}, cadastrado'
         arquivo.close()
